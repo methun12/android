a47411fe6744c1714468fbe68cad4cc899ed1482
methun12 <69069999+methun12@users.noreply.github.com>
2022-09-08 01:43:36 +0600

    Add files via upload

---
+++ devices.js
@@ -0,0 +1,351 @@
+
+var brand = {
+	a:"Samsung",
+	b:"Xaiomi",
+	c:"Nokia",
+	d:"Realme",
+	e:"Vivo",
+	f:"Oppo",
+	g:"OnePlus"
+}
+		// SAMSUNG //
+var Samsung = {
+	a: "Galaxy A13",
+	b: "Galaxy M53",
+	c: "Galaxy A23",
+	d: "Galaxy A04s",
+	e: "Galaxy A23 5G",
+	f: "Galaxy M13 5G",
+	g: "Galaxy A73 5G",
+	h: "Galaxy A53 5G",
+	i: "Galaxy A33 5G",
+	j: "Galaxy F23",
+	k: "Galaxy M33",
+	l: "Galaxy M23",
+	m: "Galaxy M52 5G",
+	n: "Galaxy M22",
+	o: "Galaxy F22",
+}
+var sm = {
+	a: "SM-A135F",
+	b: "SM-M536B",
+	c: "SM-A235F",
+	d: "SM-A047F",
+	e: "SM-A236U",
+	f: "SM-M136B",
+	g: "SM-A736B",
+	h: "SM-A536B",
+	i: "SM-A336E",
+	j: "SM-E236B",
+	k: "SM-M336B",
+	l: "SM-M236B",
+	m: "SM-M526BR",
+	n: "SM-M225FV",
+	o: "SM-E225F"
+}
+		// XAIOMI //
+
+var Xaiomi = {
+	a: "Redmi K50i",
+	b: "12 Lite",
+	c: "Poco F4",
+	d: "Poco X4 GT",
+	e: "Poco C40",
+	f: "Redmi 10 Prime",
+	g: "Black Shark 5 Pro",
+	h: "Redmi 10A",
+	i: "Redmi 10 5G",
+	j: "Redmi Note 11 Pro+ 5G",
+	k: "12 Pro",
+	l: "Redmi 10C",
+	m: "Poco M4 Pro",
+	n: "Redmi Note 11S",
+	o: "Redmi Note 11T 5G"
+}
+var mi = {
+	a: "22041216I",
+	b: "2203129G",
+	c: "22021211RG",
+	d: "22041216G",
+	e: "220333QPG",
+	f: "22011119TI",
+	g: "SHARK KTUS-H0",
+	h: "220233L2C",
+	i: "22041219G",
+	j: "21091116UG",
+	k: "2201122C",
+	l: "220333QAG",
+	m: "MZB0B5VIN",
+	n: "2201117SG",
+	o: "21091116AI"
+}
+		//NOKIA //
+var Nokia = {
+	a: "G400",
+	b: "C21 Plus",
+	c: "C21",
+	d: "C2 2nd Edition",
+	e: "G11",
+	f: "G21",
+	g: "G300",
+	h: "G50",
+	i: "C30",
+	j: "C20 Plus",
+	j: "X20",
+	l: "X10",
+	m: "G20",
+	n: "G10",
+	o: "C20"
+}
+var ta = {
+ a: "N1530DL",
+ b: "TA-1433",
+ c: "TA-1356",
+ d: "TA-1468",
+ e: "TA-1401",
+ f: "TA-1477",
+ g: "TA-1374",
+ h: "TA-1358",
+ i: "TA-1357",
+ j: "TA-1388",
+ k: "TA-1341",
+ l: "TA-1350",
+ m: "TA-1336",
+ n: "TA-1334",
+ o: "TA-1339"
+}
+		//Realme //
+var Realme = {
+	a: "9i 5G",
+	b: "GT2 Explorer Master",
+	c: "C30",
+	d: "GT Neo 3T",
+	e: "Narzo 50 5G",
+	f: "Q5 Pro",
+	g: "Q5i",
+	h: "9",
+	i: "C31",
+	j: "GT Neo 3 150W",
+	k: "Narzo 50A Prime",
+	l: "9 5G",
+	m: "9 Pro+",
+	n: "9 Pro",
+	o: "C35"
+}
+var rm = {
+	a: "RMX3612",
+	b: "RMX3551",
+	c: "RMX3581",
+	d: "RMX3372",
+	e: "RMX3572",
+	f: "RMX3372",
+	g: "RMX3574",
+	h: "RMX3521",
+	i: "RMX3501",
+	j: "RMX3563",
+	k: "RMX3516",
+	l: "RMX3388",
+	m: "RMX3392",
+	n: "RMX3471",
+	o: "RMX3511"
+}
+		// Vivo //
+var Vivo = {
+	a: "X80 Pro",
+	b: "V25 Pro",
+	c: "Y77e",
+	d: "Y77",
+	e: "Y33e",
+	f: "Y72t",
+	g: "T2",
+	h: "S15 Pro",
+	i: "T1 Pro",
+	j: "X80",
+	k: "S15e",
+	l: "Y21G",
+	m: "Y55 5G",
+	n: "V23 Pro",
+	o: "V23 5G"
+}
+var vv = {
+	a: "V2185A",
+	b: "V2158",
+	c: "V2166BA",
+	d: "V2169",
+	e: "V2166A",
+	f: "V2164A",
+	g: "V2199GA",
+	h: "V2207A",
+	i: "V2151",
+	j: "V2183A",
+	k: "V2190A",
+	l: "V2152",
+	m: "V2127",
+	n: "V2132",
+	o: "V2130"
+}
+		// OPPO //
+var Oppo = {
+	a: "A57e",
+	b: "A57s",
+	c: "Reno8 4G",
+	d: "A77 4G",
+	e: "Reno8 Pro",
+	f: "Reno8",
+	g: "Reno8 Lite",
+	h: "K10 5G",
+	i: "A77",
+	j: "A57 4G",
+	k: "F21 Pro 5G",
+	l: "F21 Pro",
+	m: "A96",
+	n: "A16e",
+	o: "A76"
+}
+var cph = {
+	a: "CPH2387",
+	b: "CPH2385",
+	c: "CPH2457",
+	d: "CPH2385",
+	e: "CPH2357",
+	f: "CPH2359",
+	g: "CPH2343",
+	h: "CPH2337",
+	i: "CPH2339",
+	j: "CPH2387",
+	k: "CPH2341",
+	l: "CPH2363",
+	m: "CPH2333",
+	n: "CPH2421",
+	o: "CPH2375"
+}
+		// oneplus //
+var OnePlus = {
+	a: "Nord N20 SE",
+	b: "Nord 2T",
+	c: "Nord N20 5G",
+	d: "10R",
+	e: "Nord CE 2 Lite 5G",
+	f: "Ace",
+	g: "Nord CE 2 5G",
+	h: "9RT 5G",
+	i: "Nord 2 5G",
+	j: "Nord N200 5G",
+	k: "Nord CE 5G",
+	l: "9 Pro",
+	m: "9R",
+	n: "9",
+	o: "Nord N10 5G"
+}
+var cpo = {
+	a: "CPH2469",
+	b: "CPH2399",
+	c: "GN2200",
+	d: "CPH2411",
+	e: "CPH2381",
+	f: "PGKM10",
+	g: "IV2201",
+	h: "MT2110",
+	i: "DN2101",
+	j: "DE2118",
+	k: "EB2101",
+	l: "LE2121",
+	m: "LE2101",
+	n: "LE2113",
+	o: "BE2029"
+}
+ function replace(num){
+ 	switch(num){
+ 		case 1:
+ 			return "a";
+ 			break;
+ 		case 2:
+ 			return "b";
+ 			break;
+ 		case 3:
+ 			return "c";
+ 			break;
+ 		case 4:
+ 			return "d";
+ 			break;
+ 		case 5:
+ 			return "e";
+ 			break;
+ 		case 6:
+ 			return "f";
+ 			break;
+ 		case 7:
+ 			return "g";
+ 			break;
+ 		case 8:
+ 			return "h";
+ 			break;
+ 		case 9:
+ 			return "i";
+ 			break;
+ 		case 10:
+ 			return "j";
+ 			break;
+ 		case 11:
+ 			return "k";
+ 			break;
+ 		case 12:
+ 			return "l";
+ 			break;
+ 		case 13:
+ 			return "m";
+ 			break;
+ 		case 14:
+ 			return "n";
+ 			break;
+ 		case 15:
+ 			return "o";
+ 			break;
+ 		default:
+ 			return "invalid argument";
+ 	}
+ }
+ function brands(bn){
+ 	return brand[replace(bn)];
+ }
+ function phoneDetails(dt,ax){
+ 	
+	var bcode;
+	var bnd;
+	switch(dt){
+		case "Samsung":
+			bnd=Samsung;
+			bcode = sm;
+			break;
+		case "Xaiomi":
+			bnd=Xaiomi;
+			bcode = mi;
+			break;
+		case "Nokia":
+			bnd=Nokia;
+			bcode = ta;
+			break;
+		case "Realme":
+			bnd=Realme;
+			bcode = rm;
+			break;
+		case "Vivo":
+			bnd=Vivo;
+			bcode = vv;
+			break;
+		case "Oppo":
+			bnd=Oppo;
+			bcode = cph;
+			break;
+		case "OnePlus":
+			bnd=OnePlus;
+			bcode = cpo;
+			break;
+		default: 
+			return "Invalid Argument";
+	}
+	
+	return a={"model":bnd[replace(ax)],"mCode":bcode[replace(ax)]};
+	
+ }
+ //alert(phoneDetails(brands(1),1).mCode)
